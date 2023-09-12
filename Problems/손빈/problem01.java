cc320342b958b74944102171ab3ae5d495a9a169
sonbean <143383607+sonbean@users.noreply.github.com>
2023-09-12 13:10:21 +0900

    Add files via upload

---
+++ Problems/손빈/problem01.java
@@ -0,0 +1,12 @@
+public class problem01 {
+    public static void main(String[] args) {
+        // 이름을 나타내는 String 객체 생성
+        
+
+        // 첫 번째 문자 출력
+        
+
+        // 마지막 문자 출력
+
+    }
+}
