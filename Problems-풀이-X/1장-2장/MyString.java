23fe4f93820d56846f84e3c5efad2a25f5d95df5
HS-hyeokjin <youngurwls@gmail.com>
2023-09-12 13:40:42 +0900

    String Problem

--- Problems-풀이-X/1장-2장/MyString.java
+++ Problems-풀이-X/1장-2장/MyString.java
@@ -7,44 +7,22 @@ public class MyString {
     public MyString(String string) {
         this.string = string;
     }
-
+    //문자열 연결
     public String myConcat(String str) {
         return this.string + str;
     }
-
+    // 문자열 길이 
     public int myLength(String str) {
-        // 2. 문자열 길이 구현
-        int length = 0;
-        char[] chars = str.toCharArray();
-        for (char ch : chars) {
-            length++;
-        }
-        return length;
+ 
     }
-
+    // 대문자 변환
     public String myToUpperCase(String str){
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch >= 'a' && ch <= 'z') {
-                ch = (char) (ch - 'a' + 'A');
-            }
-            result.append(ch);
-        }
-        return result.toString();
+
     }
 
     // 소문자 변환
     public String myToLowerCase(String str){
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (ch >= 'A' && ch <= 'Z') {
-                ch = (char) (ch - 'A' + 'a');
-            }
-            result.append(ch);
-        }
-        return result.toString();
+
     }
 
 }
