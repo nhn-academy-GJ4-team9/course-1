ba471f77b83ba7612850bf15de41d0ef38b3ec0f
HS-hyeokjin <youngurwls@gmail.com>
2023-09-12 13:17:01 +0900

    String문제

---
+++ Problems/김혁진/StringEx/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+E
+src/MyStringTest.java,7/4/74a9f375b4cd0c3c46fdd015d55c782082198cfb
+A
+src/MyString.java,a/d/ad5930fda4a4ff415345f0e5727165b670e9825f
---
+++ Problems/김혁진/StringEx/src/MyString.java
@@ -0,0 +1,31 @@
+//https://www.inf.unibz.it/~calvanese/teaching/04-05-ip/lecture-notes/uni02/node6.html
+
+public class MyString {
+
+    private String string;
+
+    public MyString(String string) {
+        this.string = string;
+    }
+
+    public String myConcat(String str) {
+        // 1. 문자열 연결 구현
+        return "";
+    }
+
+    public int myLength(String str) {
+        // 2. 문자열 길이 구현
+        return 0;
+    }
+
+    public String myToUpperCase(String str){
+        // 3. 대문자 반환 구현
+        return "";
+    }
+
+    public String myToLowerCase(String str){
+        // 4. 소문자 반환 구현
+        return "";
+    }
+
+}
