b0fe9bee6e0c48ee03bcac9fc2664d66d9c2910c
gichangee <93646339+gichangee@users.noreply.github.com>
2023-09-14 08:59:33 +0900

    Create Main_gichang.java

---
+++ Problems-풀이-X/3장/Main_gichang02/Main_gichang.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * 주어진 문자열에 "x"를 기준으로 해당 문자열을 잘라내 배열을 만든 후
+ * 사전순으로 정렬한 배열을 return 하는 solution 함수를 완성해 주세요.
+ *
+ * 
+ */
+public class Main_gichang {
+    public static void main(String[] args) {
+        System.out.println(solution("dxccxbbbxxaaaa"));
+    }
+
+    public static String[] solution(String input) {
+        String[] answer = {};
+      
+        Arrays.sort(answer);
+        return answer;
+    }
+}
