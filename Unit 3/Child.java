a03b0fae7ab6a4af0f5ad30bc58455ca86d5d035
fakedrakedog <143066707+fakedrakedog@users.noreply.github.com>
2023-09-15 17:13:05 +0900

    Delete Unit 3/Child.java

--- Unit 3/Child.java
+++
@@ -1,9 +0,0 @@
-public class Child {
-    private Parents parents;
-
-    public Child() {
-        Parents parents = new Parents();
-        parents.createParents();
-        System.out.println("Child Class 생성");
-    }
-}
