f4eebb7ec08e46b60a977eedd8ec4a3947b7bac3
fakedrakedog <143066707+fakedrakedog@users.noreply.github.com>
2023-09-15 17:04:10 +0900

    Update and rename Unit 1 & 2/Ex2F.java to Unit 1 & 2/Ex2/Ex2_1.java

--- Unit 1 & 2/Ex2/Ex2_1.java
+++ Unit 1 & 2/Ex2/Ex2_1.java
@@ -1,6 +1,6 @@
 package com.introduction.unit2;
 
-public class Ex2F {
+public class Ex2_1 {
     public static void main(String[] args) {
         String name = "Kim Hyunjoong";
         System.out.println(name.substring(0, 1) + " "+name.substring(name.length()-1,name.length()));
