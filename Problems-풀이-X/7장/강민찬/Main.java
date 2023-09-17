328976c1f2b4d6adcfe3dd6e5bc56e82711df499
Byun Ki <79976395+ByunKi@users.noreply.github.com>
2023-09-18 08:44:01 +0900

    2023.09.18 08:43

---
+++ Problems-풀이-X/7장/강민찬/Main.java
@@ -0,0 +1,30 @@
+package com.nhnacademy.problem.chapter7;
+
+public class Main {
+    private static final int[][] matrix = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+    };
+
+    public static void main(String[] args) {
+        printTransposition(matrix);
+    }
+
+    /**
+     * 상수로 주어진 행렬을 전치행렬의 형태로 출력하는 함수를 작성하시오.
+     * 
+     * 1 2 3
+     * 4 5 6
+     * 7 8 9
+     * 
+     * 1 4 7
+     * 2 5 8
+     * 3 6 9
+     * 
+     * @param matrix, 상수로 주어진 3*3 행렬
+     */
+    public static void printTransposition(int[][] matrix) {
+        // TODO 여기에 코드작성
+    }
+}
