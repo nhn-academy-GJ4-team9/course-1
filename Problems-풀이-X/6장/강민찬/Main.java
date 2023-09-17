8797b07c47798147d9c3d239e053385ac635c963
Byun Ki <79976395+ByunKi@users.noreply.github.com>
2023-09-18 08:42:12 +0900

    2023.09.18 08:42

---
+++ Problems-풀이-X/6장/강민찬/Main.java
@@ -0,0 +1,28 @@
+package com.nhnacademy.problem.chapter6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(solution("hello", 3));
+    }
+
+    /**
+     * 문자열과 정수를 받아 문자열의 각 문자를 정수만큼 반복하는 문자열을 생성하는 함수
+     * 
+     * 조건:
+     * 2 <= input.length() <= 5
+     * 2 <= num <= 10
+     * 문자열 input은 대문자로 구성됨.
+     * 
+     * 예시: hello → hhheeellllllooo
+     * 
+     * @param input, 반복될 문자들로 구성된 문자열
+     * @param num,   각 문자를 얼마만큼 반복해야하는지 표현한 정수
+     */
+    public static String solution(String input, int num) {
+        StringBuilder builder = new StringBuilder();
+        // TODO 여기에 코드를 작성
+
+        return builder.toString();
+    }
+}
