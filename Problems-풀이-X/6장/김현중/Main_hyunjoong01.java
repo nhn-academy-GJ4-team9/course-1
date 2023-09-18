50ecbf4f85532324c9f729b17118f3d7046eafef
fakedrakedog <143066707+fakedrakedog@users.noreply.github.com>
2023-09-18 13:02:32 +0900

    Update Main_hyunjoong01.java

--- Problems-풀이-X/6장/김현중/Main_hyunjoong01.java
+++ Problems-풀이-X/6장/김현중/Main_hyunjoong01.java
@@ -2,6 +2,7 @@ package com.introduction.unit6;
 
 import java.util.Scanner;
 //  문제 : 정수 n을 입력받아 구구단을 출력하는 기능을을 구현해주세요.
+//  잘못 입력 받을시 재입력을 받는 반복문을 작성해주세요.
 public class Main_hyunjoong01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
