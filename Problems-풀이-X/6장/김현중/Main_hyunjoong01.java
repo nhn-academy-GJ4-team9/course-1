94cafe81868120f6ec15ec28ae596863302922e9
fakedrakedog <143066707+fakedrakedog@users.noreply.github.com>
2023-09-18 09:34:19 +0900

    Add files via upload

---
+++ Problems-풀이-X/6장/김현중/Main_hyunjoong01.java
@@ -0,0 +1,23 @@
+package com.introduction.unit6;
+
+import java.util.Scanner;
+//  문제 : 정수 n을 입력받아 구구단을 출력하는 기능을을 구현해주세요.
+public class Main_hyunjoong01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수를 입력해주세요");
+
+        int n = sc.nextInt();
+        while (...) {
+            if (...) {
+                ...
+            } else {
+                ...
+            }
+            n = sc.nextInt();
+        }
+        sc.close();
+    
+
+    }
+}
