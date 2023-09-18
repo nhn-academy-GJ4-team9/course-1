599060cf0b05e450108e46463da0015bd90ebf38
sonbean <143383607+sonbean@users.noreply.github.com>
2023-09-18 13:37:55 +0900

    Create Main_Sbin03.java

---
+++ 5장/Main_Sbin03.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main_Sbin03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("점수를 입력하세요: ");
+        int score = scanner.nextInt();
+
+        char grade;
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("학점: " + grade);
+    }
+}
