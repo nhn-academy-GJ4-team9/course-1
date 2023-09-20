45f86c51b9929b33945626d8479d6ccd25495814
HS-hyeokjin <90303458+HS-hyeokjin@users.noreply.github.com>
2023-09-21 08:59:57 +0900

    7장 문제

---
+++ Problems-풀이-X/7장/김혁진/SeparateOddEven.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * 주어진 정수 배열에서 모든 홀수를 왼쪽에, 모든 짝수를 오른쪽에 위치시키는 메서드를 작성하세요.
+ * 위차가 바뀐 원소들의 순서는 필요 없습니다.
+ * <p>
+ * 예시:
+ * int[] array = {1, 4, 5, 8, 9, 2, 3};
+ * separateOddEven(array);
+ * // array의 결과는 {1, 5, 9, 3, 8, 2, 4} 또는 다르게 바뀐 배열
+ */
+public class SeparateOddEven {
+    public static void separateOddEven(int[] array) {
+        // 여기에 코드를 작성하세요.
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,4,5,8,9,2,3};
+        separateOddEven(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
