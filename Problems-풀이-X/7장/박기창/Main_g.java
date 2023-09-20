91c881a45699c9234ca95bf2e69be3fdad12d9c4
gichangee <93646339+gichangee@users.noreply.github.com>
2023-09-21 08:58:49 +0900

    Create Main_g.java

---
+++ Problems-풀이-X/7장/박기창/Main_g.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main_g {
+
+
+    /**
+     * 정수 1<=n<=100이 매개변수로 주어질때, n x n 크기의 배열을 return하는 함수 작성
+     * <p>
+     * n=3 ------> answer={{1,0,0},{0,1,0},{0,0,1}}
+     * n=5 ------> answer=[[1, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 1]]
+     * n=1 ------> answer=[[1]]
+     *
+     * @param n 2차원배열에 들어가는 input값
+     * @return int형 2차원배열
+     */
+    public static int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i][i] = 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString((solution(1))));
+    }
+}
