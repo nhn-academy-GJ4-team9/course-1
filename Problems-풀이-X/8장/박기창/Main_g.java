e58c461082e0e1af2be98d5c02fc14e751957d69
gichangee <93646339+gichangee@users.noreply.github.com>
2023-09-21 09:00:51 +0900

    Create Main_g.java

---
+++ Problems-풀이-X/8장/박기창/Main_g.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main_g {
+
+    /**
+     * test.txt 이라는 파일에 있는 사용자들이 가지고 있는 돈을 모두 더한 값을
+     * 리턴 해주는 함수를 작성하시오
+     *
+     * @param filename 읽어올 파일 이름
+     * @return 총 합계
+     * @throws IOException
+     */
+    public static int solution(String filename) throws IOException {
+        int sum = 0;
+
+ 
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(solution("test.txt"));
+    }
+}
