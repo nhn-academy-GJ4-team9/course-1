9ca2fecfa36e016d026e170ac98377aa9f085598
sonbean <143383607+sonbean@users.noreply.github.com>
2023-09-21 09:09:28 +0900

    Create Main_Sbin.java

---
+++ Problems-풀이-X/7장/손빈/Main_Sbin.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main_Sbin {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 2, 4, 5, 4, 6, 7, 4, 8, 9, 3, 10};
+        // ArrayList 를 활용해서 numbers에 있는 숫자들의 중복된 배열을 제거하고 출력하세요.
+
+        }
+}
