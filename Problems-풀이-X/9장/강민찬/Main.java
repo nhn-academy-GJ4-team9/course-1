9074bb2c4d7437f461229110fc1889eb1e1f27d7
Byun Ki <79976395+ByunKi@users.noreply.github.com>
2023-09-22 08:58:36 +0900

    2023.09.22 08:58

---
+++ Problems-풀이-X/9장/강민찬/Main.java
@@ -0,0 +1,31 @@
+/* 
+ * 문제: 다음 소스의 오류를 해결하고 그 이유를 기술 하시오.
+ * 
+ * 조건:
+ *  1. main() 함수를 수정하시 마시오.
+ *  2. B() 함수의 파라미터의 값을 변경하지 마시오.
+ *  3. B()의 Statement Block 내부를 수정하지 마시오.
+ */
+
+package com.nhnacademy.problems.chapter9;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            A();
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("오류 발생, 종료");
+        }
+    }
+
+    public static void A() {
+        B(22);
+    }
+
+    public static void B(int number) {
+        if (number % 2 == 0) {
+            throw new Exception("오류");
+        }
+    }
+}
