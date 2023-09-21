82ba946557269dd4d586c6b623158ccd12ce6321
sonbean <143383607+sonbean@users.noreply.github.com>
2023-09-21 09:10:12 +0900

    Create Main_Sbin02.java

---
+++ Problems-풀이-X/8장/손빈/Main_Sbin02.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.*;
+
+public class Main_Sbin02 {
+  public static void main(String[] args) throws IOException {
+    // test.txt 파일을 생성해서 그 파일에 "Hello world!" 라는 문자열을 입력하세요.
+  }
+}
