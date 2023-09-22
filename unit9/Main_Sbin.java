4ff659a39c31626dfe94cd652c1440b1426630e4
gichangee <93646339+gichangee@users.noreply.github.com>
2023-09-22 09:36:23 +0900

    Create Main_Sbin.java

---
+++ unit9/Main_Sbin.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main_Sbin {
+    // try- catch - finally 문으로 입력을 받아, 정수 판별 프로그램을 작성해 주세요.
+    // 문자열이 입력되면 "올바르지 않는 형식입니다." 로 출력하세요.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = 0;
+        try {
+            n = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("올바르지 않는 형식입니다.");
+        } finally {
+            scanner.close();
+        }
+    }
+}
