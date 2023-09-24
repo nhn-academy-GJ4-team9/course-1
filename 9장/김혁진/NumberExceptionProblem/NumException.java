8f783a26a2d21642eff1ae1836a8926c70732538
HS-hyeokjin <90303458+HS-hyeokjin@users.noreply.github.com>
2023-09-25 00:18:15 +0900

    9장 풀이

---
+++ 9장/김혁진/NumberExceptionProblem/NumException.java
@@ -0,0 +1,31 @@
+package hyeokjin5;
+
+/**
+ * String 을 정수로 변환 하는 메서드를 작성하세요
+ * 정수로 변환이 불능할 경우 예외 처리하고 프로그램을 중지합니다.
+ * 출력결과는 다음과 같습니다.
+ * =====================================
+ * 정수로 변환된 값은 3
+ * 정수로 변환된 값은 14
+ * 3.141592는 정수로 변환 할 수 없습니다.
+ * ====================================
+ */
+
+public class NumException {
+    public static void solution(String[] arr) {
+        int i = 0;
+        try {
+            for (i = 0; i < arr.length; i++) {
+                int j = Integer.parseInt(arr[i]);
+                System.out.println("정수로 변환된 값은 ");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(arr[i] + "는 정수로 변환 할 수 없습니다.");
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] arr = {"3", "14", "3.141592", "998"};
+        solution(arr);
+    }
+}
---
+++ 9장/박기창/throwProblem/Main_g.java
@@ -0,0 +1,31 @@
+package gichang5;
+
+public class Main_g extends Exception {
+
+
+    public Main_g(String message) {
+        super(message);
+    }
+
+    public static void main(String[] args) {
+        try {
+            test(-1);
+        } catch (Main_g e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * input값에 음수값이 들어가면 Main_g 예외가 발생하도록
+     * test 함수를 수정
+     *
+     * @param input
+     * @throws Main_g
+     */
+    public static void test(int input) throws Main_g {
+        if (input < 0) {
+            throw new Main_g("음수값은 들어올 수 없습니다.");
+        }
+    }
+
+}
