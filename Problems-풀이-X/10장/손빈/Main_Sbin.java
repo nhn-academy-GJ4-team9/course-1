3850f7fd2e7dcced2f6b259d2b8c7507e49a8dd2
sonbean <143383607+sonbean@users.noreply.github.com>
2023-09-25 09:11:00 +0900

    Create Main_Sbin.java

---
+++ Problems-풀이-X/10장/손빈/Main_Sbin.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// int값으로 정수를 받아 재귀 함수로 팩토리얼 계산하는 프로그램을 작성해주세요.
+
+public class Main_Sbin {
+    public static void main(String[] args) {
+
+    }
+
+    public static int calculateFactorial(int n) {
+        
+    }
+}
+
